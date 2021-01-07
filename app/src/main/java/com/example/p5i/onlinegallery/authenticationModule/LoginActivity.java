f6cd157556028e1da38809daaa3e7a0b315d3b51
fares <mokhtariothmanefares@gmail.com>
2021-01-07 17:04:24 +0100

    adding error handler on the view

--- app/src/main/java/com/example/p5i/onlinegallery/authenticationModule/LoginActivity.java
+++ app/src/main/java/com/example/p5i/onlinegallery/authenticationModule/LoginActivity.java
@@ -11,11 +11,13 @@ import android.view.View;
 
 import com.example.p5i.onlinegallery.R;
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class LoginActivity extends AppCompatActivity {
     private static final String TAG = "LoginActivity";
     private MaterialButton materialButton;
     private AnimatedVectorDrawable animatedVectorDrawable;
+    private TextInputLayout textInputLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +25,14 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         materialButton=findViewById(R.id.materialButton);
         animatedVectorDrawable=(AnimatedVectorDrawable) materialButton.getIcon();
-
+        textInputLayout=findViewById(R.id.textInputLayout);
 
         materialButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 animatedVectorDrawable.start();
                 materialButton.setText("next");
+                textInputLayout.setError("someting wen wrrong");
             }
         });
     }
