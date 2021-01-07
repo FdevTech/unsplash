efc44154ee2b9afeeb4e1bff2ee4b25df16c818e
fares <mokhtariothmanefares@gmail.com>
2021-01-07 10:34:02 +0100

    starting designing our app

---
+++ app/src/main/java/com/example/p5i/onlinegallery/authenticationModule/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.p5i.onlinegallery.authenticationModule;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.p5i.onlinegallery.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
