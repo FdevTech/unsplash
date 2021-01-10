78abe39d7dd38db79e6ba85a693193914b5ac6da
fares <mokhtariothmanefares@gmail.com>
2021-01-10 10:48:53 +0100

    the authorization call to get the access tocken has been done

---
+++ app/src/main/java/com/example/p5i/onlinegallery/authenticationModule/AutorizationResponsePJO.java
@@ -0,0 +1,44 @@
+package com.example.p5i.onlinegallery.authenticationModule;
+
+public class AutorizationResponsePJO
+{
+    private String access_token,token_type,scope;
+    private int created_at;
+
+    public AutorizationResponsePJO()
+    {
+
+    }
+
+    public String getAccess_token() {
+        return access_token;
+    }
+
+    public void setAccess_token(String access_token) {
+        this.access_token = access_token;
+    }
+
+    public String getToken_type() {
+        return token_type;
+    }
+
+    public void setToken_type(String token_type) {
+        this.token_type = token_type;
+    }
+
+    public String getScope() {
+        return scope;
+    }
+
+    public void setScope(String scope) {
+        this.scope = scope;
+    }
+
+    public int getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(int created_at) {
+        this.created_at = created_at;
+    }
+}
--- app/src/main/java/com/example/p5i/onlinegallery/authenticationModule/LoginActivity.java
+++ app/src/main/java/com/example/p5i/onlinegallery/authenticationModule/LoginActivity.java
@@ -1,10 +1,19 @@
 package com.example.p5i.onlinegallery.authenticationModule;
 
 import androidx.appcompat.app.AppCompatActivity;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
+import android.content.Intent;
 import android.graphics.drawable.AnimatedVectorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.p5i.onlinegallery.R;
 import com.google.android.material.button.MaterialButton;
@@ -18,6 +27,12 @@ public class LoginActivity extends AppCompatActivity {
     private ExtendedFloatingActionButton materialButton;
     private AnimatedVectorDrawable animatedVectorDrawable;
     private TextInputLayout emailTextInputLayout,passwordTextInputLayout;
+    Intent intent;
+    String url="https://unsplash.com/oauth/authorize?client_id=CH5YIV_t-PtFB52Db4bAXGQxiQEVy79ZTy9wa4z90iQ&redirect_uri=curta://callback&response_type=code" +
+            "&scope=public read_user write_user read_photos write_photos write_likes write_followers read_collections write_collections";
+
+    private Retrofit retrofit;
+    private AutorizationInterface mAutorizationInterface;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,13 +42,24 @@ public class LoginActivity extends AppCompatActivity {
         animatedVectorDrawable=(AnimatedVectorDrawable) materialButton.getIcon();
         emailTextInputLayout =findViewById(R.id.emailTextInputLayout);
         passwordTextInputLayout=findViewById(R.id.passwordTextInputLayout);
-        materialButton.setOnClickListener(new View.OnClickListener() {
+
+        retrofit=new Retrofit.Builder()
+                   .baseUrl("https://unsplash.com")
+                   .addConverterFactory(GsonConverterFactory.create())
+                   .build();
+
+        mAutorizationInterface=retrofit.create(AutorizationInterface.class);
+
+        intent=new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                materialButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                animatedVectorDrawable.start();
-                materialButton.shrink();
-                emailTextInputLayout.setError("someting went wrrong");
-                passwordTextInputLayout.setError("someting went wrrong");
+
+                startActivity(intent);
+               // animatedVectorDrawable.start();
+              //  materialButton.shrink();
+              //  emailTextInputLayout.setError("someting went wrrong");
+               // passwordTextInputLayout.setError("someting went wrrong");
             }
         });
     }
@@ -41,6 +67,29 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Uri mUri=getIntent().getData();
+        if(mUri!=null)
+        {
+            Toast.makeText(this,"we got it "+ mUri.getQueryParameter("code"),Toast.LENGTH_LONG).show();
+            mAutorizationInterface.getAccessTocken("CH5YIV_t-PtFB52Db4bAXGQxiQEVy79ZTy9wa4z90iQ","yUWMA9JU_1ZuLmwRbnkwDSx1cI3TKktQTK8x2eAC-dk",
+                    "curta://callback",mUri.getQueryParameter("code"),"authorization_code").enqueue(new Callback<AutorizationResponsePJO>() {
+                @Override
+                public void onResponse(Call<AutorizationResponsePJO> call, Response<AutorizationResponsePJO> response) {
+                    Log.d(TAG, "onResponse: "+response.body().getAccess_token());
+                }
+
+                @Override
+                public void onFailure(Call<AutorizationResponsePJO> call, Throwable t) {
+
+                }
+            });
+        }
 
     }
 }
