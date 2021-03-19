53774e1bc8add406a3d9fef8e7c983b8a8b159e1
fares <mokhtariothmanefares@gmail.com>
2021-03-19 19:59:08 +0100

    collection entity collection retreived and stored in dataabse collection recycler view done topic done profile done profile done navigation from bottom view done onclick recycler view done

--- app/src/main/java/com/example/p5i/onlinegallery/photosModule/PhotosListFragment.kt
+++ app/src/main/java/com/example/p5i/onlinegallery/photosModule/PhotosListFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -42,7 +43,7 @@ class PhotosListFragment : Fragment() {
           photoViewModel=ViewModelProvider(this,photoViewModelFactory).get(PhotoViewModel::class.java)
 
           photoViewModelAdapter=PhotoViewModelAdapter(OnPhotoClickListner {
-
+             Toast.makeText(this.context,"${it.id}",Toast.LENGTH_SHORT).show()
               Log.d(TAG, "onCreateView: ${it.id}")
           })
 
