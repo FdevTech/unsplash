a8e6f413e8d810f414e27c684e35b1e8fa44477c
fares <mokhtariothmanefares@gmail.com>
2021-03-24 11:44:31 +0100

    the design of the collection_item.xml has been upgraded hashing was implemented well fixing the problem between topics and app I have to fix the hash memory problem

--- app/src/main/java/com/example/p5i/onlinegallery/photoModule/viewmodel/SinglePhotoDataBinging.kt
+++ app/src/main/java/com/example/p5i/onlinegallery/photoModule/viewmodel/SinglePhotoDataBinging.kt
@@ -3,12 +3,25 @@ package com.example.p5i.onlinegallery.photoModule.viewmodel
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.example.p5i.onlinegallery.collectionsModule.viewModel.getSecpond
 import com.example.p5i.onlinegallery.photosModule.domain.PhotoDomain
+import xyz.belvi.blurhash.BlurHash
+import xyz.belvi.blurhash.blurPlaceHolder
 
 @BindingAdapter("image")
 fun ImageView.setImage(photo: PhotoDomain)
 {
-    Glide.with(this.context)
+    //todo fix the problem of memory
+    val blurHash: BlurHash = BlurHash(this.context, lruSize = 20, punch = 1F)
+    photo.blur_hash?.let {
+        Glide.with(context)
+            .load(photo.photo_regular)
+            .blurPlaceHolder(it,this, blurHash)
+            { requestBuilder ->
+                requestBuilder.into(this)
+            }
+    }
+   /* Glide.with(this.context)
         .load(photo.photo_regular)
-        .into(this)
+        .into(this)*/
 }
