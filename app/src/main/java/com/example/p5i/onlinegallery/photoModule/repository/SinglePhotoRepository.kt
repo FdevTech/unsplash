bc16b0ac43dc0d1a706d8a1bba48deeec4f897a6
fares <mokhtariothmanefares@gmail.com>
2021-03-24 14:22:46 +0100

    everything is working just fine

--- app/src/main/java/com/example/p5i/onlinegallery/photoModule/repository/SinglePhotoRepository.kt
+++ app/src/main/java/com/example/p5i/onlinegallery/photoModule/repository/SinglePhotoRepository.kt
@@ -1,21 +1,29 @@
 package com.example.p5i.onlinegallery.photoModule.repository
 
+import android.util.Log
 import androidx.lifecycle.Transformations
 import com.example.p5i.onlinegallery.databse.UnsplashDatabase
 import com.example.p5i.onlinegallery.photosModule.datalayer.network.photosDatbase.asDomainModel
 import com.example.p5i.onlinegallery.photosModule.datalayer.network.photosDatbase.asDomainModelFromCollectionPhoto
 import com.example.p5i.onlinegallery.photosModule.datalayer.network.photosDatbase.asDomainModelFromTopicPhoto
 
+private const val TAG = "SinglePhotoRepository"
 class SinglePhotoRepository (private val unsplashDatabase: UnsplashDatabase)
 {
-    val photos= Transformations.map(unsplashDatabase.photosDao.getAllPhotos(),{
+    val photos= Transformations.map(unsplashDatabase.photosDao.getAllPhotos(),
+        {
+            Log.d(TAG, "getAllPhotos: ${it.size} ")
         it.asDomainModel()
     })
-    val photosTopic= Transformations.map(unsplashDatabase.photosDao.getAllPhotosFromTopic(),{
+    val photosTopic= Transformations.map(unsplashDatabase.photosDao.getAllPhotosFromTopic(),
+        {
+            Log.d(TAG, "getAllPhotosFromTopic: ${it.size}")
         it.asDomainModelFromTopicPhoto()
     })
     val photosCollection=
-        Transformations.map(unsplashDatabase.photosDao.getAllPhotosFromCollection(),{
+        Transformations.map(unsplashDatabase.photosDao.getAllPhotosFromCollection(),
+            {
+                Log.d(TAG, "getAllPhotosFromCollection: ${it.size}")
         it.asDomainModelFromCollectionPhoto()
     })
 }
